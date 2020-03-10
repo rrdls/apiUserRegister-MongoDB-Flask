5a623d0e3f6c088128d0283018c5ac9ed182705e
renato <rramosdls.developer@gmail.com>
2020-03-10 15:31:23 -0300

    v.1. Add cryptography to password

--- CONTROLLERS/usersController.py
+++ CONTROLLERS/usersController.py
@@ -5,17 +5,29 @@ import datetime
 from copy import deepcopy
 import json
 from flask import jsonify
+import bcrypt
 
 VALIDATE = Validator(require_all=True).validate
 USER = CLIENT['user']
 
 
+def cryptography(pwd):
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(pwd.encode(), salt)
+    return hashed
+
+
 def registerController(data):
     dataWithoutDate = deepcopy(data)
     data['date'] = datetime.datetime.utcnow()
+    data['pwd'] = cryptography(data['pwd']).decode("utf-8")
     if VALIDATE(data, userSchema) == True:
-        USER.insert_one(data)
-        return dataWithoutDate, 201
+        email = [user['email'] for user in usersController()[0]]
+        if data['email'] not in email:
+            USER.insert_one(data)
+            return dataWithoutDate, 201
+        else:
+            return {"message": "Email already exists"}, 400
     else:
         return {"error": "Invalid data"}, 400
 
--- MODELS/usersModel.py
+++ MODELS/usersModel.py
@@ -3,6 +3,6 @@
 userSchema = {
     "name": {"type": "string", 'empty': False},
     "email": {"type": "string",  'regex': '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'},
-    "pwd": {"type": "string", "minlength": 6, "maxlength": 16},
+    "pwd": {"type": "string"},
     "date": {"type": "date"}
 }
